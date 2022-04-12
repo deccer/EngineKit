c6f7d51fcda168bbb4c3bbbaa578267fc41c4551
deccer <decccer@outlook.com>
2022-04-13 00:04:20 +0200

    Add GL bindings project

---
+++ src/EngineKit.Native.OpenGL/GL.Enums.cs
@@ -0,0 +1,12 @@
+﻿namespace EngineKit.Native.OpenGL
+{
+    public static partial class GL
+    {
+        public enum FramebufferTarget : uint
+        {
+            ReadFramebuffer = 0x8CA8,
+            DrawFramebuffer = 0x8CA9,
+            Framebuffer = 0x8D40
+        }
+    }
+}
