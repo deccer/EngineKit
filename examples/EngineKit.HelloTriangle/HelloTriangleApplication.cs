47bd5eb4250b2d67391dd90f67f71ddfb7c932c3
deccer <decccer@outlook.com>
2022-04-13 00:04:57 +0200

    Add Example project consuming OpenGL

---
+++ examples/EngineKit.HelloTriangle/HelloTriangleApplication.cs
@@ -0,0 +1,23 @@
+﻿using EngineKit.Native.OpenGL;
+using Serilog;
+
+namespace EngineKit.HelloWindow
+{
+    internal class HelloWindowApplication : Application
+    {
+        public HelloWindowApplication(ILogger logger)
+            : base(logger)
+        {
+        }
+
+        protected override bool Load()
+        {
+            return base.Load();
+        }
+
+        protected override void Render()
+        {
+            GL.BindFramebuffer(GL.FramebufferTarget.Framebuffer, 0);
+        }
+    }
+}
---
+++ examples/EngineKit.HelloTriangle/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace EngineKit.HelloWindow;
+
+public static class Program
+{
+    public static void Main()
+    {
+        using var serviceProvider = CreateServiceProvider();
+        using var application = serviceProvider.GetRequiredService<IApplication>();
+
+        application.Run();
+    }
+
+    private static ServiceProvider CreateServiceProvider()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", false)
+            .Build();
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+
+        var services = new ServiceCollection();
+        services.AddSingleton(configuration);
+        services.AddSingleton(Log.Logger);
+        services.AddSingleton<IApplication, HelloWindowApplication>();
+        return services.BuildServiceProvider();
+    }
+}
