5ed08e2707bff4857d8a97d417d9739c1ca33cd2
deccer <decccer@outlook.com>
2022-09-04 01:18:59 +0200

    Initial application skellington

---
+++ examples/HelloWindow/Program.cs
@@ -0,0 +1,29 @@
+﻿using EngineKit;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace HelloWindow;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        using var serviceProvider = CreateServiceProvider();
+
+        var application = serviceProvider.GetRequiredService<IApplication>();
+        application.Run();
+    }
+
+    private static ServiceProvider CreateServiceProvider()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        var services = new ServiceCollection();
+        services.AddSingleton(Log.Logger);
+        services.AddSingleton<IApplication, HelloWindowApplication>();
+        return services.BuildServiceProvider();
+    }
+}
