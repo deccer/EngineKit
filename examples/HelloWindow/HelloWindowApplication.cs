0145d61ec007153a8a8c94bdf6bef0968c45de42
deccer <decccer@outlook.com>
2022-09-13 01:58:23 +0200

    Update example application

--- examples/HelloWindow/HelloWindowApplication.cs
+++ examples/HelloWindow/HelloWindowApplication.cs
@@ -1,5 +1,9 @@
 using EngineKit;
+using EngineKit.Graphics;
+using EngineKit.Input;
 using EngineKit.Native.Glfw;
+using EngineKit.Native.OpenGL;
+using Microsoft.Extensions.Options;
 using Serilog;
 
 namespace HelloWindow;
@@ -7,15 +11,43 @@ namespace HelloWindow;
 internal sealed class HelloWindowApplication : Application
 {
     private readonly ILogger _logger;
+    private readonly IGraphicsContext _graphicsContext;
 
     public HelloWindowApplication(
         ILogger logger,
+        IOptions<WindowSettings> windowSettings,
+        IOptions<ContextSettings> contextSettings,
         IApplicationContext applicationContext,
         IMetrics metrics,
-        IInputProvider inputProvider)
-        : base(logger, applicationContext, metrics, inputProvider)
+        IInputProvider inputProvider,
+        IGraphicsContext graphicsContext)
+        : base(logger, windowSettings, contextSettings, applicationContext, metrics, inputProvider)
     {
         _logger = logger;
+        _graphicsContext = graphicsContext;
+    }
+
+    protected override bool Load()
+    {
+        if (!base.Load())
+        {
+            _logger.Error("{Category}: Unable to load", "App");
+            return false;
+        }
+
+        GL.ClearColor(0.4f, 0.2f, 0.1f, 1.0f);
+        return true;
+    }
+
+    protected override void Render()
+    {
+        GL.Clear(GL.ClearBufferMask.ColorBufferBit | GL.ClearBufferMask.DepthBufferBit);
+    }
+
+    protected override void Unload()
+    {
+        _graphicsContext.Dispose();
+        base.Unload();
     }
 
     protected override void Update()
---
+++ src/EngineKit/MathHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EngineKit;
+
+public static class MathHelper
+{
+    public static float ToRadians(float degrees)
+    {
+        return degrees * MathF.PI / 180f;
+    }
+}
