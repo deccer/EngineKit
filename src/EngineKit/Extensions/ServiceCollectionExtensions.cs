c8983b3a8020b5959d59a1ea80f457966018b387
deccer <decccer@outlook.com>
2022-09-25 01:40:51 +0200

    Register UIRenderer

--- src/EngineKit/Extensions/ServiceCollectionExtensions.cs
+++ src/EngineKit/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 using EngineKit.Graphics;
 using EngineKit.Input;
+using EngineKit.UI;
 using Microsoft.Extensions.DependencyInjection;
 using SixLabors.ImageSharp;
 
@@ -25,6 +26,8 @@ public static class ServiceCollectionExtensions
         services.AddSingleton<IImageLibrary, ImageLibrary>();
         services.AddSingleton<IApplicationContext, ApplicationContext>();
         services.AddSingleton<IInputProvider, InputProvider>();
+
+        services.AddSingleton<IUIRenderer, UIRenderer>();
 
         return services;
     }
