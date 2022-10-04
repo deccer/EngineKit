fa37c19d6850aae6c5d901581126e82ae39603db
deccer <decccer@outlook.com>
2022-10-04 12:32:09 +0200

    Rename GraphicsPipelineDescriptorBuilder to GraphicsPipelineBuilder

--- src/EngineKit/Extensions/ServiceCollectionExtensions.cs
+++ src/EngineKit/Extensions/ServiceCollectionExtensions.cs
@@ -19,7 +19,7 @@ public static class ServiceCollectionExtensions
 
         services.AddSingleton<IFramebufferFactory, FramebufferFactory>();
         services.AddSingleton<IGraphicsContext, GraphicsContext>();
-        services.AddSingleton<IGraphicsPipelineDescriptorBuilder, GraphicsPipelineDescriptorBuilder>();
+        services.AddSingleton<IGraphicsPipelineBuilder, GraphicsPipelineBuilder>();
         services.AddSingleton<ITextureLoader, TextureLoader>();
         services.AddSingleton<ITextureLibrary, TextureLibrary>();
         services.AddSingleton<IMaterialLibrary, MaterialLibrary>();
