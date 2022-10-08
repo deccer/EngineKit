331634c0eb3953cbc5062d7c7ac1558651eed760
deccer <decccer@outlook.com>
2022-10-08 22:24:02 +0200

    Include ImGui's shaders in EngineKit.Assets

---
+++ src/EngineKit.Assets/Shaders/ImGui.vs.glsl
@@ -0,0 +1,26 @@
+#version 460 core
+#extension GL_ARB_separate_shader_objects : enable
+#extension GL_ARB_explicit_uniform_location : enable
+
+layout(location = 0) in vec2 in_position;
+layout(location = 1) in vec2 in_uv;
+layout(location = 3) in vec4 in_color;
+
+out gl_PerVertex
+{
+    vec4 gl_Position;
+};
+layout(location = 1) out vec4 fs_color;
+layout(location = 2) out vec2 fs_uv;
+
+layout(std140, binding = 0) uniform GlobalMatrices
+{
+    mat4 ProjectionMatrix;
+};
+
+void main()
+{
+    gl_Position = ProjectionMatrix * vec4(in_position, 0, 1);
+    fs_color = in_color;
+    fs_uv = in_uv;
+}
