f0bd0a547c050ddd99e9813dbad252adb7e13e19
deccer <decccer@outlook.com>
2022-10-24 21:55:36 +0200

    Replace FramebufferRenderDescriptorBuilder.WithColorAttachments with .WithColorAttachment to add individual framebuffer attachments
    previous one didnt allow to set individual clear flags per attachment

--- src/EngineKit/Graphics/FramebufferRenderDescriptorBuilder.cs
+++ src/EngineKit/Graphics/FramebufferRenderDescriptorBuilder.cs
@@ -1,14 +1,17 @@
+using System.Collections.Generic;
 using EngineKit.Mathematics;
 
 namespace EngineKit.Graphics;
 
 public sealed class FramebufferRenderDescriptorBuilder
 {
+    private readonly IList<RenderAttachment> _renderAttachments;
     private FramebufferRenderDescriptor _framebufferRenderDescriptor;
 
     public FramebufferRenderDescriptorBuilder()
     {
         _framebufferRenderDescriptor = new FramebufferRenderDescriptor();
+        _renderAttachments = new List<RenderAttachment>();
     }
 
     public FramebufferRenderDescriptorBuilder WithViewport(int width, int height)
@@ -17,33 +20,34 @@ public sealed class FramebufferRenderDescriptorBuilder
         return this;
     }
 
-    public FramebufferRenderDescriptorBuilder WithColorAttachments(Vector4 clearColor, bool clear, params ITexture[] colorTextures)
+    public FramebufferRenderDescriptorBuilder WithColorAttachment(ITexture colorAttachment, bool clear,
+        Vector4 clearColor)
     {
         var clearValue = new ClearValue(clearColor.X, clearColor.Y, clearColor.Z, clearColor.W, 1.0f, 0);
-        _framebufferRenderDescriptor.ColorAttachments = new RenderAttachment[colorTextures.Length];
-        for (var colorAttachmentIndex = 0; colorAttachmentIndex < colorTextures.Length; colorAttachmentIndex++)
-        {
-            _framebufferRenderDescriptor.ColorAttachments[colorAttachmentIndex] =
-                new RenderAttachment(colorTextures[colorAttachmentIndex], clearValue, clear);
-        }
+        _renderAttachments.Add(new RenderAttachment(colorAttachment, clearValue, clear));
 
         return this;
     }
 
-    public FramebufferRenderDescriptorBuilder WithDepthAttachment(ITexture depthTexture, bool clear)
+    public FramebufferRenderDescriptorBuilder WithDepthAttachment(ITexture depthTexture, bool clear, float clearValue = 1.0f)
     {
-        _framebufferRenderDescriptor.DepthAttachment = new RenderAttachment(depthTexture, ClearValue.White, clear);
+        _framebufferRenderDescriptor.DepthAttachment = new RenderAttachment(depthTexture, new ClearValue(0, 0, 0, 0, clearValue, 0), clear);
         return this;
     }
 
-    public FramebufferRenderDescriptorBuilder WithStencilAttachment(ITexture stencilTexture, bool clear)
+    public FramebufferRenderDescriptorBuilder WithStencilAttachment(ITexture stencilTexture, bool clear, int clearValue = 0)
     {
-        _framebufferRenderDescriptor.StencilAttachment = new RenderAttachment(stencilTexture, ClearValue.Zero, clear);
+        _framebufferRenderDescriptor.StencilAttachment = new RenderAttachment(stencilTexture, new ClearValue(0, 0, 0, 0, 0.0f, clearValue), clear);
         return this;
     }
 
     public FramebufferRenderDescriptor Build(string label)
     {
+        _framebufferRenderDescriptor.ColorAttachments = new RenderAttachment[_renderAttachments.Count];
+        for (var i = 0; i < _renderAttachments.Count; i++)
+        {
+            _framebufferRenderDescriptor.ColorAttachments[i] = _renderAttachments[i];
+        }
         _framebufferRenderDescriptor.Label = label;
         return _framebufferRenderDescriptor;
     }
