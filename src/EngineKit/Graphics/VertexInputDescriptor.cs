b67ecec31d19436124d6a329734127a19427acce
deccer <decccer@outlook.com>
2022-10-25 17:11:48 +0200

    Remove not needed methods

--- src/EngineKit/Graphics/VertexInputDescriptor.cs
+++ src/EngineKit/Graphics/VertexInputDescriptor.cs
@@ -1,54 +1,8 @@
-using System;
-
 namespace EngineKit.Graphics;
 
-public readonly record struct VertexInputDescriptor
+public readonly record struct VertexInputDescriptor(VertexInputBindingDescriptor[] VertexBindingDescriptors, Label Label)
 {
-    public readonly VertexInputBindingDescriptor[] VertexBindingDescriptors;
+    public readonly VertexInputBindingDescriptor[] VertexBindingDescriptors = VertexBindingDescriptors;
 
-    public static VertexInputDescriptor CreateFromVertexType(VertexType vertexType)
-    {
-        switch (vertexType)
-        {
-            case VertexType.Default:
-                return new VertexInputDescriptor(null);
-            case VertexType.ImGui:
-                return new VertexInputDescriptor(
-                    new VertexInputBindingDescriptor(0, 0, 2, DataType.Float, 0),
-                    new VertexInputBindingDescriptor(1, 0, 2, DataType.Float, 8),
-                    new VertexInputBindingDescriptor(3, 0, 4, DataType.UnsignedByte, 16, true));
-            case VertexType.Position:
-                return VertexInputDescriptorFactory.CreateFromStruct<VertexPosition>();
-            case VertexType.PositionColor:
-                return VertexInputDescriptorFactory.CreateFromStruct<VertexPositionColor>();
-            case VertexType.PositionColorUv:
-                return VertexInputDescriptorFactory.CreateFromStruct<VertexPositionColorUv>();
-            case VertexType.PositionNormal:
-                return VertexInputDescriptorFactory.CreateFromStruct<VertexPositionNormal>();
-            case VertexType.PositionNormalUv:
-                return VertexInputDescriptorFactory.CreateFromStruct<VertexPositionNormalUv>();
-            case VertexType.PositionNormalUvTangent:
-                return VertexInputDescriptorFactory.CreateFromStruct<VertexPositionNormalUvTangent>();
-            case VertexType.PositionUv:
-                return VertexInputDescriptorFactory.CreateFromStruct<VertexPositionUv>();
-            default:
-                throw new ArgumentOutOfRangeException(nameof(vertexType));
-        }
-    }
-
-    public VertexInputDescriptor(params VertexInputBindingDescriptor[]? vertexBindingDescriptors)
-    {
-        if (vertexBindingDescriptors != null)
-        {
-            VertexBindingDescriptors = new VertexInputBindingDescriptor[vertexBindingDescriptors.Length];
-            for (var i = 0; i < vertexBindingDescriptors.Length; i++)
-            {
-                VertexBindingDescriptors[i] = vertexBindingDescriptors[i];
-            }
-        }
-        else
-        {
-            VertexBindingDescriptors = Array.Empty<VertexInputBindingDescriptor>();
-        }
-    }
+    public readonly Label Label = Label;
 }
