da982cffc1ce0eee5e6f09df3ca603b13c1f2ca7
deccer <decccer@outlook.com>
2022-11-13 16:07:45 +0100

    New Example

---
+++ examples/Swapchain/Swapchain.Assets/Shaders/Scene.vs.glsl
@@ -0,0 +1,34 @@
+#version 460 core
+
+layout(location = 0) in vec3 i_position;
+layout(location = 1) in vec3 i_normal;
+layout(location = 2) in vec2 i_uv;
+
+layout(location = 0) out vec3 v_position;
+layout(location = 1) out vec3 v_normal;
+layout(location = 2) out vec2 v_uv;
+
+layout(binding = 0, std140) uniform GpuConstants
+{
+    mat4 ViewProj;
+};
+
+struct GpuObject
+{
+    mat4 Model;
+};
+
+layout(binding = 1, std430) readonly buffer ObjectBuffer
+{
+    ObjectUniforms Objects[];
+} objectBuffer;
+
+void main()
+{
+    int i = gl_InstanceID + gl_BaseInstance;
+    ObjectUniforms object = objectBuffer.Objects[i];
+    v_position = (object.Model * vec4(i_position, 1.0)).xyz;
+    v_normal = normalize(inverse(transpose(mat3(object.Model))) * i_normal);
+    v_uv = i_uv;
+    gl_Position = ViewProj * vec4(v_position, 1.0);
+}
