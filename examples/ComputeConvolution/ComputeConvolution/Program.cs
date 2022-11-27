45085fc55359f16df43100ea69723fc66f3544b1
deccer <decccer@outlook.com>
2022-11-27 15:51:51 +0100

    [WIP] Add compute pipeline example

---
+++ examples/ComputeConvolution/ComputeConvolution/Program.cs
@@ -0,0 +1,38 @@
+﻿using EngineKit;
+using EngineKit.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace ComputeConvolution;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        using var serviceProvider = CreateServiceProvider();
+
+        var application = serviceProvider.GetRequiredService<IApplication>();
+        application.Run();
+    }
+
+    private static ServiceProvider CreateServiceProvider()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", false)
+            .Build();
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+
+        var services = new ServiceCollection();
+        services.AddSingleton(configuration);
+        services.AddSingleton(Log.Logger);
+        services.Configure<WindowSettings>(configuration.GetSection(nameof(WindowSettings)));
+        services.Configure<ContextSettings>(configuration.GetSection(nameof(ContextSettings)));
+        services.AddEngine();
+        services.AddSingleton<IApplication, ComputeConvolutionApplication>();
+        return services.BuildServiceProvider();
+    }
+}
