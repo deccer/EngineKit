fd6be96db784347017fb2cdb3207aa368ff84606
deccer <decccer@outlook.com>
2023-01-04 00:02:21 +0100

    Set Count when allocating with elements

--- src/EngineKit/Graphics/Buffer.cs
+++ src/EngineKit/Graphics/Buffer.cs
@@ -36,6 +36,7 @@ internal abstract class Buffer : IBuffer
         where TElement : unmanaged
     {
         GL.NamedBufferStorage(Id, element, storageAllocationFlags.ToGL());
+        Count = 1;
         SizeInBytes = Marshal.SizeOf<TElement>();
     }
 
@@ -43,7 +44,8 @@ internal abstract class Buffer : IBuffer
         where TElement : unmanaged
     {
         GL.NamedBufferStorage(Id, elements, storageAllocationFlags.ToGL());
-        SizeInBytes = Marshal.SizeOf<TElement>() * elements.Length;
+        Count = elements.Length;
+        SizeInBytes = Marshal.SizeOf<TElement>() * Count;
     }
 
     public void Update(nint dataPtr, int offsetInBytes, int sizeInBytes)
@@ -99,72 +101,3 @@ internal class Buffer<TElement> : Buffer
         };
     }
 }
-
-/*
-internal class Buffer<TElement> : Buffer
-    where TElement : unmanaged
-{
-    internal Buffer(
-        BufferTarget bufferTarget,
-        Label label,
-        TElement data)
-        : this(bufferTarget, label)
-    {
-        GL.NamedBufferStorage(Id, data, GL.BufferStorageMask.DynamicStorageBit | GL.BufferStorageMask.MapWriteBit);
-        Stride = Unsafe.SizeOf<TElement>();
-        Count = 1;
-    }
-
-    internal Buffer(
-        BufferTarget bufferTarget,
-        Label label,
-        TElement[] data)
-        : this(bufferTarget, label)
-    {
-        GL.NamedBufferStorage(Id, data, GL.BufferStorageMask.DynamicStorageBit | GL.BufferStorageMask.MapWriteBit);
-        Count = data.Length;
-        Stride = Unsafe.SizeOf<TElement>();
-    }
-
-    internal Buffer(
-        BufferTarget bufferTarget,
-        Label label,
-        uint size)
-        : this(bufferTarget, label)
-    {
-        Stride = Unsafe.SizeOf<TElement>();
-        unsafe
-        {
-            GL.NamedBufferStorage(
-                Id,
-                (uint)(size * sizeof(TElement)),
-                nint.Zero,
-                GL.BufferStorageMask.DynamicStorageBit | GL.BufferStorageMask.MapWriteBit);
-        }
-    }
-
-    private Buffer(BufferTarget bufferTarget, Label? label = null)
-    {
-        var innerLabel = $"{GetBufferNamePrefix(bufferTarget)}_{typeof(TElement).Name}";
-        if (!string.IsNullOrEmpty(label))
-        {
-            innerLabel += $"_{label}";
-        }
-
-        GL.ObjectLabel(GL.ObjectIdentifier.Buffer, Id, innerLabel);
-    }
-
-    private static string GetBufferNamePrefix(BufferTarget bufferTarget)
-    {
-        return bufferTarget switch
-        {
-            BufferTarget.VertexBuffer => "Buffer_VB",
-            BufferTarget.IndexBuffer => "Buffer_IB",
-            BufferTarget.ShaderStorageBuffer => "Buffer_SS",
-            BufferTarget.UniformBuffer => "Buffer_UB",
-            BufferTarget.IndirectDrawBuffer => "Buffer_ID",
-            _ => throw new ArgumentOutOfRangeException(nameof(bufferTarget), bufferTarget, null)
-        };
-    }
-}
-*/
