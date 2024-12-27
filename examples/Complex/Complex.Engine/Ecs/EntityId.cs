13bc2f9075cf5f8bb17d0560bc4156f7128f7453
deccer <decccer@outlook.com>
2024-12-27 04:09:28 +0100

    Turn EntityId into a record

--- examples/Complex/Complex.Engine/Ecs/EntityId.cs
+++ examples/Complex/Complex.Engine/Ecs/EntityId.cs
@@ -1,28 +1,3 @@
-using System;
-
 namespace Complex.Engine.Ecs;
 
-public readonly struct EntityId : IEquatable<EntityId>
-{
-    private readonly int _entityId;
-
-    public EntityId(int entityId)
-    {
-        _entityId = entityId;
-    }
-
-    public bool Equals(EntityId other)
-    {
-        return _entityId == other._entityId;
-    }
-
-    public override bool Equals(object? obj)
-    {
-        return obj is EntityId other && Equals(other);
-    }
-
-    public override int GetHashCode()
-    {
-        return _entityId;
-    }
-}
+public readonly record struct EntityId(int Id);
